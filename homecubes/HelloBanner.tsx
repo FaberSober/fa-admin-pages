6bccbf1766155687d1698d62489c75a9a9ed22c0
xu.pengfei <1508075252@qq.com>
2023-01-09 22:48:29 +0800

    home page cube grid editing

--- homecubes/HelloBanner.tsx
+++ homecubes/HelloBanner.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { UserLayoutContext } from "@features/fa-admin-pages/layout";
+import { FaUtils } from "@fa/ui";
+import { useInterval } from "ahooks";
 
 
 export interface HelloBannerProps {
 }
 
 export function HelloBanner() {
-  return <div>HelloBanner</div>;
+  const {user} = useContext(UserLayoutContext)
+  const [time, setTime] = useState(FaUtils.getCurDateTime())
+
+  useInterval(() => {
+    setTime(FaUtils.getCurDateTime())
+  }, 1000)
+
+  return (
+    <div style={{ padding: 12 }}>
+      <div style={{fontSize: '18px', fontWeight: 600}} className="fa-mb12">Hello, {user.name}.</div>
+      <div>{time}</div>
+    </div>
+  );
 }
 
 HelloBanner.displayName = "HelloBanner";
