75599fe99657246d18049c645e70528559a2b0a8
xu.pengfei <1508075252@qq.com>
2023-02-27 17:16:13 +0800

    feat: disk-文件上传进度悬浮框展示

--- layout/menu/MenuContainer.tsx
+++ layout/menu/MenuContainer.tsx
@@ -1,7 +1,8 @@
 import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { ApiEffectLayout, PageLoading } from '@fa/ui';
-import { LangLayout, UserLayout } from "@/layout";
+import LangLayout from "@features/fa-admin-pages/layout/lang/LangLayout";
+import UserLayout from "@features/fa-admin-pages/layout/user/UserLayout";
 import MenuLayout from "./MenuLayout";
 
 import 'allotment/dist/style.css';
