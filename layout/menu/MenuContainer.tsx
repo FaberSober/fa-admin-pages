8ca37918832cb681c2772b32bc672632b1374679
xupengfei <faberxu@gmail.com>
2023-04-21 11:40:46 +0800

    feat: demo-map-amap-basic

--- layout/menu/MenuContainer.tsx
+++ layout/menu/MenuContainer.tsx
@@ -4,22 +4,27 @@ import { ApiEffectLayout, PageLoading } from '@fa/ui';
 import LangLayout from "../lang/LangLayout";
 import UserLayout from "../user/UserLayout";
 import MenuLayout from "./MenuLayout";
+import { APILoader } from '@uiw/react-amap';
 
 import 'allotment/dist/style.css';
 
+const VITE_APP_AMAP_KEY = import.meta.env.VITE_APP_AMAP_KEY;
 
 export default function MenuContainer() {
   return (
-    <LangLayout>
-      <ApiEffectLayout>
-        <UserLayout>
-          <MenuLayout>
-            <Suspense fallback={<PageLoading />}>
-              <Outlet />
-            </Suspense>
-          </MenuLayout>
-        </UserLayout>
-      </ApiEffectLayout>
-    </LangLayout>
+    // @ts-ignore
+    <APILoader version="2.0.5" akey={VITE_APP_AMAP_KEY}>
+      <LangLayout>
+        <ApiEffectLayout>
+          <UserLayout>
+            <MenuLayout>
+              <Suspense fallback={<PageLoading />}>
+                <Outlet />
+              </Suspense>
+            </MenuLayout>
+          </UserLayout>
+        </ApiEffectLayout>
+      </LangLayout>
+    </APILoader>
   );
 }
