861fcbcdc66189fcb5e97d92717e5de636f1840c
xupengfei <faberxu@gmail.com>
2023-04-21 15:34:20 +0800

    feat: demo-map-amap-basic-使用geocoder做地理/逆地理编码

---
+++ components/map/AMapSearchSelect.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export interface AMapSearchSelectProps {
+  // onSelect?:
+}
+
+/**
+ * @author xu.pengfei
+ * @date 2023/4/21 15:08
+ */
+export default function AMapSearchSelect({}: AMapSearchSelectProps) {
+  return (
+    <div></div>
+  )
+}
