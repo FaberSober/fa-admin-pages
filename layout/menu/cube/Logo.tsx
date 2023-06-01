6b0cb5df41a41bc3fc2dcea60aadabc13486b472
xupengfei <faberxu@gmail.com>
2023-06-01 14:00:52 +0800

    feat: base-分页方式循环获取数据

--- layout/menu/cube/Logo.tsx
+++ layout/menu/cube/Logo.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { fileSaveApi } from '@/services';
 import { UserLayoutContext } from "@/layout";
+import { SITE_INFO } from '@/configs';
 
 
 /**
@@ -11,7 +12,7 @@ import { UserLayoutContext } from "@/layout";
 export default function Logo() {
   const { systemConfig } = useContext(UserLayoutContext);
   return (
-    <Link to="/admin" className="fa-menu-logo">
+    <Link to={SITE_INFO.HOME_LINK} className="fa-menu-logo">
       <img src={fileSaveApi.genLocalGetFilePreview(systemConfig.logo)} alt="logo" className="fa-menu-logo-img" />
       <span className="fa-menu-logo-title">{systemConfig.title}</span>
     </Link>
