8a2dfddddce851feae6281327845ac251f20f61e
xu.pengfei <1508075252@qq.com>
2023-06-26 22:10:48 +0800

    feat: base-代码整理

--- components/helper/UserSearchSelect.tsx
+++ components/helper/UserSearchSelect.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BaseSearchSelect, BaseSearchSelectProps } from '@fa/ui';
 import { Admin } from '@/types';
-import { userApi } from '@/services';
+import { userApi as api } from '@/services';
 
 export interface UserSearchSelectProps extends Omit<BaseSearchSelectProps<Admin.User, string>, 'serviceApi'> {}
 
@@ -15,9 +15,9 @@ export default function UserSearchSelect(props: UserSearchSelectProps) {
       valueKey="id"
       labelKey="name"
       serviceApi={{
-        search: (searchValue) => userApi.page({ current: 1, pageSize: 20, query: { name: searchValue } }),
-        getById: (value) => userApi.getById(value),
-        findList: (ids) => userApi.list({ 'id#$in': [...ids] }),
+        search: (searchValue) => api.page({ current: 1, pageSize: 20, query: { name: searchValue } }),
+        getById: (value) => api.getById(value),
+        findList: (ids) => api.list({ 'id#$in': [...ids] }),
       }}
       placeholder="请输入员工名称进行搜索"
       {...props}
