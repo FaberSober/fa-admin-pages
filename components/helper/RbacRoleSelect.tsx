822ca5c8e0f175b12ec717c8ff12d19206db064a
xu.pengfei <1508075252@qq.com>
2023-06-26 22:00:26 +0800

    feat: base-代码整理

--- components/helper/RbacRoleSelect.tsx
+++ components/helper/RbacRoleSelect.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BaseSelect, BaseSelectProps } from '@fa/ui';
 import { Rbac } from '@/types';
-import { rbacRoleApi } from '@/services';
+import { rbacRoleApi as api } from '@/services';
 
 /**
  * @author xu.pengfei
  * @date 2022/9/28
  */
 export default function RbacRoleSelect({ ...props }: Omit<BaseSelectProps<Rbac.RbacRole>, 'serviceApi'>) {
-  return <BaseSelect serviceApi={rbacRoleApi} placeholder="请选择角色" {...props} />;
+  return <BaseSelect serviceApi={api} placeholder="请选择角色" {...props} />;
 }
