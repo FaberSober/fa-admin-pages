3767c962e214c3dec41feee3e0e0ac562b1fd4c2
xu.pengfei <1508075252@qq.com>
2023-09-05 21:52:46 +0800

    base-更新用户角色

--- pages/h5/in.tsx
+++ pages/h5/in.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { PageLoading } from "@fa/ui";
+import { PageLoading, setToken, useQs } from "@fa/ui";
 import { Outlet } from "react-router-dom";
 import UserLayout from "../../layout/user/UserLayout";
 
@@ -10,6 +10,10 @@ import UserLayout from "../../layout/user/UserLayout";
  * @date 2023/9/5 19:33
  */
 export default function index() {
+  const search:any = useQs();
+
+  setToken(search.token)
+
   return (
     <UserLayout>
       <Suspense fallback={<PageLoading/>}>
