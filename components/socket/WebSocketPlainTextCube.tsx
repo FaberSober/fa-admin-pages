cee4db7f11e818ca246844566cf32afd6b9faa64
xupengfei <faberxu@gmail.com>
2024-12-11 22:04:43 +0800

    base-update

--- components/socket/WebSocketPlainTextCube.tsx
+++ components/socket/WebSocketPlainTextCube.tsx
@@ -1,5 +1,6 @@
 import React, { CSSProperties, useState } from 'react';
 import useBus from "use-bus";
+import { FaUtils } from "@fa/ui";
 
 
 export interface WebSocketPlainTextCubeProps {
@@ -18,12 +19,13 @@ export default function WebSocketPlainTextCube({ style, className }: WebSocketPl
     ['@@ws/RECEIVE/PLAIN_TEXT'],
     ({ payload }) => {
       setArray([ ...array, payload ])
+      FaUtils.scrollToBottomById('WebSocketPlainTextCube', 100)
     },
     [array],
   )
 
   return (
-    <div style={{ maxHeight: 400, overflowY: 'auto', ...style }} className={className}>
+    <div id="WebSocketPlainTextCube" style={{ maxHeight: 400, width: '100%', overflowY: 'auto', ...style }} className={className}>
       {array.map(item => <div key={item}>{item}</div>)}
     </div>
   )
