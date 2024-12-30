f86463e3f0352801b141308c08722d5b689b2f5e
xu.pengfei <1508075252@qq.com>
2024-12-30 15:06:21 +0800

    base-FileBizListView.tsx

--- components/file/FileBizListView.tsx
+++ components/file/FileBizListView.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Admin } from "@features/fa-admin-pages/types";
-import { PaperClipOutlined } from "@ant-design/icons";
-import { fileSaveApi } from "@features/fa-admin-pages/services";
+import FileSaveIcon from "./FileSaveIcon";
+import { FaUtils } from "@fa/ui";
 
 
 export interface FileBizListViewProps {
@@ -18,13 +18,15 @@ export default function FileBizListView({ fileBizList = [] }: FileBizListViewPro
     <div className="fa-flex-column">
       {fileBizList.map(item => {
         return (
-          <a
+          <FileSaveIcon
             key={item.id}
-            href={fileSaveApi.genLocalGetFile(item.fileId)}
-          >
-            <PaperClipOutlined />
-            <span>{item.fileName}</span>
-          </a>
+            file={{
+              id: item.fileId,
+              originalFilename: item.fileName,
+              ext: FaUtils.getExtension(item.fileName),
+            }}
+            width={25}
+          />
         )
       })}
     </div>
