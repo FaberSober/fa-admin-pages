6bee500019e8768266e74a5a3e510f34296ad0cf
xu.pengfei <1508075252@qq.com>
2025-07-17 16:40:20 +0800

    admin-定时任务-下拉框-增加搜索功能

--- pages/admin/system/base/job/modal/JobModal.tsx
+++ pages/admin/system/base/job/modal/JobModal.tsx
@@ -90,7 +90,13 @@ export default function JobModal({ children, title, record, fetchFinish, addBtn,
           }}
         >
           <Form.Item name="clazzPath" label="任务执行方法" rules={[{ required: true }]} {...FaUtils.formItemFullLayout}>
-            <Select options={jobs} />
+            <Select
+              options={jobs}
+              showSearch
+              filterOption={(input, option) =>
+                (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+              }
+            />
           </Form.Item>
           <Form.Item name="jobName" label="任务名称" rules={[{ required: true }]} {...FaUtils.formItemFullLayout}>
             <Input />
