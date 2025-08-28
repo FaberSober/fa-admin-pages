c64233d9029fd93be13a9d3ecd45e61fd3698285
xu.pengfei <1508075252@qq.com>
2025-08-28 21:59:33 +0800

    admin-add FaApiScrollList

--- components/infinite-scroll/FaApiScrollList.tsx
+++ components/infinite-scroll/FaApiScrollList.tsx
@@ -1,5 +1,7 @@
 import { Fa } from '@fa/ui';
-import React, { CSSProperties } from 'react';
+import React, { CSSProperties, useEffect, useId, useState } from 'react';
+import { Divider, Skeleton } from 'antd';
+import InfiniteScroll from './InfiniteScroll';
 
 export interface FaApiScrollListProps<T> {
   /** page分页获取接口 */
@@ -17,8 +19,61 @@ export interface FaApiScrollListProps<T> {
  * @author xu.pengfei
  * @date 2025-08-28 21:39:55
  */
-export default function FaApiScrollList<T>({}: FaApiScrollListProps<T>) {
+export default function FaApiScrollList<T>({ apiPage, renderItem, style }: FaApiScrollListProps<T>) {
+  const id = useId();
+  const [loading, setLoading] = useState(false);
+  const [pagination, setPagination] = useState<Fa.Pagination>();
+  const [data, setData] = useState<T[]>([]);
+  const [page, setPage] = useState(1);
+
+  function loadMoreData() {
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    apiPage({
+      query: {},
+      current: page,
+      pageSize: 20,
+      sorter: 'id DESC'
+    }).then(res => {
+      setData([...data, ...res.data.rows]);
+      setPage(page + 1);
+      setPagination(res.data.pagination);
+      setLoading(false);
+    }).catch(() => setLoading(false));
+  }
+
+  useEffect(() => {
+    loadMoreData();
+  }, []);
+
+  const defaultStyle: CSSProperties = {
+    height: '100%',
+    overflow: 'auto',
+    padding: '0 12px',
+    // border: '1px solid rgba(140, 140, 140, 0.35)',
+  };
+
   return (
-    <InfiniteScroll></InfiniteScroll>
+    <div
+      id={id}
+      style={{ ...defaultStyle, ...style }}
+    >
+      <InfiniteScroll
+        dataLength={data.length}
+        next={loadMoreData}
+        hasMore={pagination ? pagination.hasNextPage : true}
+        loader={<Skeleton avatar paragraph={{ rows: 1 }} active />}
+        endMessage={<Divider plain>数据加载完成～</Divider>}
+        scrollableTarget={id}
+      >
+        {data.map((item, index) => (
+          <div key={index}>
+            {renderItem(item)}
+          </div>
+        ))}
+      </InfiniteScroll>
+    </div>
   );
 }
--- components/infinite-scroll/index.ts
+++ components/infinite-scroll/index.ts
@@ -1,1 +1,2 @@
 export { default as InfiniteScroll } from './InfiniteScroll'
+export { default as FaApiScrollList } from './FaApiScrollList'
