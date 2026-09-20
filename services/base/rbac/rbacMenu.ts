292223774e0593a482bf5c7d6e57f2dca43effec
xu.pengfei <1508075252@qq.com>
2026-09-20 09:53:38 +0800

    chore(menu): format rbac menu service

--- services/base/rbac/rbacMenu.ts
+++ services/base/rbac/rbacMenu.ts
@@ -1,13 +1,11 @@
-import { GATE_APP } from '@/configs';
 import { BaseTreeApi } from '@fa/ui';
+import { GATE_APP } from '@/configs';
 import type { Fa, Rbac } from '@/types';
 
 /** ------------------------------------------ xx 操作接口 ------------------------------------------ */
 class RbacMenuApi extends BaseTreeApi<Rbac.RbacMenu, string> {
-
   /** 查询流程菜单列表 */
   getFlowMenuList = (): Promise<Fa.Ret<Fa.Option[]>> => this.get('getFlowMenuList');
-
 }
 
 export default new RbacMenuApi(GATE_APP.rbac, 'rbacMenu');
